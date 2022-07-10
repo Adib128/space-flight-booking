ef5ed21886f76edc29a084b6cc579577d0324c1c
Adib128 <aouadi.adib@gmail.com>
2022-07-10 10:43:25 +0100

    Authentication errors modified

--- src/auth.js
+++ src/auth.js
@@ -1,12 +1,16 @@
-const auth = ({ ctx }) => {
+const { ForbiddenError, AuthenticationError } = require("apollo-server-koa");
 
+const auth = ({ ctx }) => {
+    if (!ctx.headers.authorization) {
+        throw new ForbiddenError("Unauthorized");
+    }
     const header = ctx.headers.authorization;
     const token = header.split(" ")[1];
 
     const testToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
 
     if (token != testToken) {
-        throw new Error("Authentication error");
+        throw new AuthenticationError("Authentication error");
     }
 };
 
